0975babc45a76f314c987b9c887ebefe4673ef25
Nate Dobbins <hello@natedobbins.net>
2016-01-22 18:42:03 +0800

    add basic stdio logging

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
 	//	"io/ioutil"
+	// "html/template"
+
+	// github.com/spf13/viper for reading config and content front matter
 )
 
 func main() {
-	exec.Command("hugo").Output()                                                        // rebuild site content
+	fmt.Println("Compiling website...")
+	exec.Command("hugo").Output() // rebuild site content
+	fmt.Println("Website compiled.")
+
 	http.Handle("/", http.FileServer(http.Dir("./public")))                              // serve site output (`/public`) into `/`
 	http.Handle("/admin/", http.StripPrefix("/admin/", http.FileServer(http.Dir("./")))) // serve site input (`/`) into `/admin`
-	err := http.ListenAndServe(":3000", nil)                                             // start server on port 3000
+
+	fmt.Println("Starting server on port 3000...")
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
