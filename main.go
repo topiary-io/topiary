192a20222b8a643464b4c0cce9c5fbbe94038f4f
Nate Dobbins <hello@natedobbins.net>
2016-01-22 22:06:19 +0800

    move hugo compilation to separate func

--- main.go
+++ main.go
@@ -11,10 +11,14 @@ import (
 	// github.com/spf13/viper for reading config and content front matter
 )
 
-func main() {
+func buildSite() {
 	fmt.Println("Compiling website...")
-	exec.Command("hugo").Output() // rebuild site content
+	exec.Command("hugo")
 	fmt.Println("Website compiled.")
+}
+
+func main() {
+	buildSite()
 
 	http.Handle("/", http.FileServer(http.Dir("./public")))                              // serve site output (`/public`) into `/`
 	http.Handle("/admin/", http.StripPrefix("/admin/", http.FileServer(http.Dir("./")))) // serve site input (`/`) into `/admin`
