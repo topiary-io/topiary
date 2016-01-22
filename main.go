09ac7b49d4704c2f80cb7c1687d9786486221027
Nate Dobbins <hello@natedobbins.net>
2016-01-23 02:08:27 +0800

    list dirs and files in admin, edit and save files

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os/exec"
-	//	"io/ioutil"
 	// "html/template"
-
 	// github.com/spf13/viper for reading config and content front matter
 )
 
@@ -17,11 +16,66 @@ func buildSite() {
 	fmt.Println("Website compiled.")
 }
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/admin/"):]
+	paths, _ := ioutil.ReadDir(title)
+	for _, p := range paths {
+		if p.IsDir() {
+			fmt.Fprintf(w, "<div><a href=\"%s/\">%s</a></div>", p.Name(), p.Name())
+		} else {
+			fmt.Fprintf(w, "<div>edit: <a href=\"/edit/%s%s\">%s</a></div>", title, p.Name(), p.Name())
+		}
+	}
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/admin/", http.StatusFound)
+}
+
 func main() {
 	buildSite()
 
-	http.Handle("/", http.FileServer(http.Dir("./public")))                              // serve site output (`/public`) into `/`
-	http.Handle("/admin/", http.StripPrefix("/admin/", http.FileServer(http.Dir("./")))) // serve site input (`/`) into `/admin`
+	http.Handle("/", http.FileServer(http.Dir("./public"))) // serve site output (`/public`) into `/`
+	http.HandleFunc("/admin/", adminHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 
 	fmt.Println("Starting server on port 3000...")
 	err := http.ListenAndServe(":3000", nil)
