e3b02eaa3d1dc73d3f0764007c81244fc321b59c
Nate Dobbins <hello@natedobbins.net>
2016-01-25 15:01:09 +0800

    move edit and save admin functionality under /admin location

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func (p *Page) save() error {
 func git(file string) {
 	//need to accommodate for username, email eventually
 	cmdName := "git"
-	gitAdd := []string {"add", file}
+	gitAdd := []string{"add", file}
 	outadd, err := exec.Command(cmdName, gitAdd...).Output()
 	if err != nil {
 		fmt.Printf("fatal error on git add\n")
@@ -40,9 +40,9 @@ func git(file string) {
 	}
 	fmt.Printf("\n------git------\ngit add "+file+" %s\n", outadd)
 
-	gitCommit := []string {"commit", "-m", "updating "+file+" from topiary"}
+	gitCommit := []string{"commit", "-m", "updating " + file + " from topiary"}
 	outci, err := exec.Command(cmdName, gitCommit...).Output()
-	if err !=nil {
+	if err != nil {
 		fmt.Printf("fatal error on git commit\n")
 		fmt.Println(err)
 	}
@@ -59,7 +59,8 @@ func loadPage(title string) (*Page, error) {
 }
 
 func adminHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len(getConfig("AdminLocation")):]
+	adminLocation := getConfig("AdminLocation")
+	title := r.URL.Path[len(adminLocation):]
 	if title == "" {
 		title = "./"
 	}
@@ -68,20 +69,20 @@ func adminHandler(w http.ResponseWriter, r *http.Request) {
 		if p.IsDir() {
 			fmt.Fprintf(w, "<div><a href=\"%s/\">%s</a></div>", p.Name(), p.Name())
 		} else {
-			fmt.Fprintf(w, "<div>edit: <a href=\"/edit/%s%s\">%s</a></div>", title, p.Name(), p.Name())
+			fmt.Fprintf(w, "<div>edit: <a href=\""+adminLocation+"edit/%s%s\">%s</a></div>", title, p.Name(), p.Name())
 		}
 	}
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
 	adminLocation := getConfig("AdminLocation")
+	title := r.URL.Path[len(adminLocation+"edit/"):]
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
 	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<form action=\""+adminLocation+"save/%s\" method=\"POST\">"+
 		"<textarea name=\"body\" style=\"width:800px;height:400px;\">%s</textarea><br>"+
 		"<input type=\"submit\" value=\"Save\">"+
 		"</form>"+
@@ -90,13 +91,14 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	adminLocation := getConfig("AdminLocation")
+	title := r.URL.Path[len(adminLocation+"save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	p.save()
 	git(title) //this should probably only run if there have been changes
-	buildSite()	
-	http.Redirect(w, r, getConfig("AdminLocation"), http.StatusFound)
+	buildSite()
+	http.Redirect(w, r, adminLocation, http.StatusFound)
 }
 
 func main() {
@@ -107,8 +109,8 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./public"))) // serve site output (`/public`) into `/`
 	http.HandleFunc(adminLocation, adminHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc(adminLocation+"edit/", editHandler)
+	http.HandleFunc(adminLocation+"save/", saveHandler)
 
 	fmt.Println("Starting server on port 3000...")
 	fmt.Println("Admin available at:", adminLocation)
